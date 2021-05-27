f1118fddc83a1ff0b57ebbdae0f98f0a6b53dca7
Player Ghost <playerghost13@gmail.com>
2021-05-27 18:56:42 -0300

    .

--- src/app/page/profissionais/profissionais.component.ts
+++ src/app/page/profissionais/profissionais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { Usuario } from 'src/app/entidades/usuario';
 import { ProfissionaisService } from 'src/app/services/profissionais.service';
@@ -12,8 +13,9 @@ export class ProfissionaisComponent implements OnInit {
   isLoggedIn$: Observable<boolean>;
   user: Usuario
   public profissionais: Usuario[]
+  form: FormGroup
 
-  constructor(private profissionaisService: ProfissionaisService) { 
+  constructor(private formBuilder: FormBuilder, private profissionaisService: ProfissionaisService) { 
     this.profissionais = []
   }
 
@@ -26,8 +28,35 @@ export class ProfissionaisComponent implements OnInit {
       this.updateUserPlaylists()
     }*/
 
+    this.form = this.formBuilder.group({
+      "nome": new FormControl('', Validators.required),
+      "sobrenome": new FormControl('', Validators.required),
+      "email": new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      "cep": new FormControl('', Validators.required),
+    });
+
     this.profissionaisService.getProfissionais().subscribe((profissionais: Usuario[]) => {
       this.profissionais = profissionais
     })
+  }
+
+  submit(){
+
+  }
+
+  get nome() {
+    return this.form.get('nome')
+  }
+
+  get sobrenome() {
+    return this.form.get('sobrenome')
+  }
+
+  get email() {
+    return this.form.get('email')
+  }
+
+  get cep() {
+    return this.form.get('cep')
   }
 }
